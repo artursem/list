c5938e24445bdba9afd65fa25b446a3cd6871314
artursem <artur.semeniuk@gmail.com>
2021-04-27 19:54:20 +0200

    add promises

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const fs = require('util');
+const util = require('util');
 
-const lstat = util.promisify(fs.lstat);
+// METHOD #2
+// const lstat = util.promisify(fs.lstat);
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// METHOD #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
-        // handle error
         console.log(err);
     };
     
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
 
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        console.log(filenames[index], stats.isFile());
+    };
 });
 
+// METHOD #1
 // const lstat = (filename) => {
 //     return new Promise((resolve, reject) => {
 //         fs.lstat(filename, (err, stats) => {

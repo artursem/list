9ab849ff2a39e5738eb5d4e22daf57b56d61adc8
artursem <artur.semeniuk@gmail.com>
2021-04-27 19:18:57 +0200

    add files and folders 1

--- index.js
+++ index.js
@@ -2,11 +2,31 @@
 
 const fs = require('fs');
 
+
 fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         // handle error
         console.log(err);
     };
+    const allStats = Array(filenames.length).fill(null);
+    for (let filename of filenames){
+            
+        const index = filenames.indexOf(filename);
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                console.log(err);
+            };
+            
+            allStats[index] = stats;
 
-    console.log(filenames);
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(filenames[index], stats.isFile());
+                });
+            };
+        });
+    };
 });
